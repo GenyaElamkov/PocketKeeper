4e31528a7491bc98690fc54cd813cddcd3e06989
Evgeny Elamkov <GenyaElamkov@gmail.com>
2026-03-30 16:46:56 +0300

    create update_account

--- src/api/accounts.py
+++ src/api/accounts.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.db_depends import get_async_db
@@ -8,6 +8,7 @@ from src.models.accounts import Account as AccountModel
 from src.models.users import User as UserModel
 from src.schemas.accounts import Account as AccountSchema
 from src.schemas.accounts import AccountsCreate as AccountsCreateSchema
+from src.schemas.accounts import AccountUpdate as AccountUpdateSchema
 
 router = APIRouter(
     prefix="/accounts",
@@ -56,9 +57,32 @@ async def get_all_accounts(db: AsyncSession = Depends(get_async_db),
     return db_accounts
 
 
-@router.put("/{account_id}", name="Обновить счет")
-async def update_account(account_id: int) -> dict:
-    return {"message": f"Счет с ID {account_id} обновлена (заглушка)"}
+@router.put("/{account_id}", name="Обновить счет", response_model=AccountSchema)
+async def update_account(account_id: int,
+                         account: AccountUpdateSchema,
+                         db: AsyncSession = Depends(get_async_db),
+                         current_user: UserModel = Depends(get_current_member),
+                         ) -> AccountSchema:
+    result = await db.scalars(
+        select(AccountModel).where(AccountModel.id == account_id),
+    )
+    db_account = result.first()
+    if not db_account:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Счет не найден",
+        )
+    if db_account.user_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Недостаточно прав для обновления счета",
+        )
+    await db.execute(
+        update(AccountModel).where(AccountModel.id == account_id).values(**account.model_dump()),
+    )
+    await db.commit()
+    await db.refresh(db_account)
+    return db_account
 
 
 @router.delete("/{account_id}", name="Удалить счет", response_model=AccountSchema)
--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -46,3 +46,19 @@ class Account(AccountsCreate):
     )
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class AccountUpdate(BaseModel):
+    """Модель для обновления счета"""
+    name: str | None = Field(None,
+                             description="Название счета (например, 'Кошелек')",
+                             max_length=100,
+                             )
+    type: AccountType | None = Field(..., description="Тип счета (cash, card, deposit)") # noqa
+    currency: AccountCurrency | None = Field(..., description="Код валюты (RUB, USD)")
+    balance: Decimal | None = Field(None,
+                                    description="Текущий баланс",
+                                    max_digits=10,
+                                    decimal_places=2,
+                                    )
+    is_archived: bool | None = Field(None, description="Архивирован ли счет (скрыт из выбора)")
