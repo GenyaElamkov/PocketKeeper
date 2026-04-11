504f58b4c3dd0b7a61c7f99a32253a53689c626c
Evgeny Elamkov <GenyaElamkov@gmail.com>
2026-04-11 14:53:28 +0300

    add fun get_current_dt

--- src/models/common.py
+++ src/models/common.py
@@ -1,13 +1,24 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from sqlalchemy import DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 
+def get_current_dt() -> datetime:
+    """Получить текущую дату и время"""
+    return datetime.now(tz=timezone.utc)
+
+
 class TimeBase:
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=func.now(), nullable=False,
+        DateTime(timezone=True),
+        default=get_current_dt,
+        server_default=func.now(),
+        nullable=False,
     )
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=func.now(), onupdate=func.now(), nullable=False,
+        DateTime(timezone=True),
+        default=get_current_dt,
+        onupdate=func.now(),
+        nullable=False,
     )
