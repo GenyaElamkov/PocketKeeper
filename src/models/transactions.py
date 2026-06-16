4c57f268e4db9f8d755721bc765470752b14ab3b
Evgeny Elamkov <GenyaElamkov@gmail.com>
2026-06-16 17:58:39 +0300

    Add Index in Transaction

--- src/models/transactions.py
+++ src/models/transactions.py
@@ -2,7 +2,7 @@ from datetime import date
 from decimal import Decimal
 from typing import TYPE_CHECKING
 
-from sqlalchemy import BOOLEAN, DATE, DECIMAL, ForeignKey, String
+from sqlalchemy import BOOLEAN, DATE, DECIMAL, ForeignKey, Index, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.database import Base
@@ -17,11 +17,18 @@ if TYPE_CHECKING:
 class Transaction(Base, TimeBaseMixin):
     __tablename__ = "transactions"
 
+    __table_args__ = (
+        Index("ix_transactions_user_id", "user_id"),
+        Index("ix_transactions_account_id", "account_id"),
+        Index("ix_transactions_category_id", "category_id"),
+        Index("ix_transactions_transaction_date", "transaction_date"),
+    )
+
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     account_id: Mapped[int] = mapped_column(ForeignKey("accounts.id"), nullable=False)
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
     amount: Mapped[Decimal] = mapped_column(DECIMAL(10, 2), nullable=False)
-    type: Mapped[str] = mapped_column(nullable=False)   # noqa
+    transaction_type: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str | None] = mapped_column(String(200), nullable=True)
     transaction_date: Mapped[date] = mapped_column(DATE, nullable=False)
     is_active: Mapped[bool] = mapped_column(BOOLEAN, default=True)
